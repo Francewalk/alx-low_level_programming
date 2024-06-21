cf688c098779527523907925678fd6cf66805245
Francewalk <francewalk062@gmail.com>
2024-06-20 22:46:56 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A programmer that prints a line which puts fuction
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
