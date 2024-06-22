c64f421e9e804294869c57f2885eba78fa270733
Francewalk <francewalk062@gmail.com>
2024-06-22 07:45:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,11 @@
 
 /**
 *
-*
-* Return: Always 1 (sucees)
+* Return: Always 1 (Sucees)
 */
 int main(void)
 {
 	write(2,
-	"and the piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
