7409e9128ca61c9c28f1c12eb31408b90f25640d
Francewalk <francewalk062@gmail.com>
2024-06-22 07:37:51 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+*
+*
+* Return: Always 1 (sucees)
+*/
+int main(viod)
+{
+	write(2,
+	"and the piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
